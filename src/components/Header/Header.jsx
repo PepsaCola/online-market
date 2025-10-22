eabd21d677015f80a7bf923a3d0933ea344e246d
mogkolya <kv149q52@gmail.com>
2025-10-22 17:22:09 +0300

    added redirection from the main page to favorite

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { AccountIcon, BasketIcon, Container, LikeIcon, Title, Wrap } from './styled';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <Container>
       <Title>MARKET</Title>
       <Wrap>
-        <LikeIcon width={40} height={35} />
+        <Link to="/favorite">
+          <LikeIcon width={40} height={35} />
+        </Link>
         <BasketIcon width={40} height={39} />
         <AccountIcon width={38} height={40} />
       </Wrap>
